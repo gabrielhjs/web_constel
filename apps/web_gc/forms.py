ee8f475c8bf45101a293163dc038ef8ac80488d8
Gabriel Sa <gabrieljensensa@gmail.com>
2019-10-24 21:00:42 -0300

    note

--- apps/web_gc/forms.py
+++ apps/web_gc/forms.py
@@ -1,5 +1,8 @@
-from django import forms
+from django.forms import ModelForm
+from apps.web_gc.models import Talao
 
 
-class NameForm(forms.Form):
-    your_name = forms.CharField(label='Your name', max_length=100)
+class FormTalao(ModelForm):
+    class Meta:
+        model = Talao
+        fields = ['talao', ]
--- apps/web_gc/views.py
+++ apps/web_gc/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
-from .forms import NameForm
+from .forms import FormTalao
+from .models import CadastroTalao, Talao
 
 
 def view_index(request):
@@ -12,16 +13,17 @@ def get_name(request):
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
-        form = NameForm(request.POST)
+        form = FormTalao(request.POST)
         # check whether it's valid:
         if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return HttpResponseRedirect('/gc')
+            form.save()
+            talao = Talao.objects.get(talao=form.data.get('talao'))
+            cadastro = CadastroTalao(talao=talao)
+            cadastro.save()
+            return HttpResponseRedirect('')
 
     # if a GET (or any other method) we'll create a blank form
     else:
-        form = NameForm()
+        form = FormTalao()
 
     return render(request, 'web_gc/cadastro_talao.html', {'form': form})
