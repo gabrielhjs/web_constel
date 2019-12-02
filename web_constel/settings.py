595786d467b40a6faec597f837bbcba89939f1f4
Gabriel <gabrieljensensa@gmail.com>
2019-12-02 14:46:16 -0300

    corrigindo bug

--- web_constel/settings.py
+++ web_constel/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['constel.herokuapp.com', 'http://192.168.1.114:8080', 'constel.tk', ]
 
