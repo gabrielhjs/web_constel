22431cbfa20b88a9c82ad82723c85909d21e9383
Gabriel Sa <gabrieljensensa@gmail.com>
2020-04-07 04:10:46 -0300

    acertando api do cont

--- apps/almoxarifado/apps/cont/api/view_sets.py
+++ apps/almoxarifado/apps/cont/api/view_sets.py
@@ -1,5 +1,4 @@
 from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.views import APIView
 from rest_framework.settings import api_settings
 from rest_framework import generics, authentication, permissions, status
 from rest_framework.decorators import api_view
@@ -8,6 +7,7 @@ from rest_framework.response import Response
 from django.contrib.auth import get_user_model
 
 from .serializers import AuthTokenSerializer, UserSerializer, SerializerOntBaixa
+from ..models import OntSaida
 
 
 class CreateTokenView(ObtainAuthToken):
@@ -26,15 +26,7 @@ class ViewUsers(generics.ListAPIView):
     serializer_class = UserSerializer
 
 
-class ViewOntBaixa(generics.GenericAPIView):
-    authentication_classes = (authentication.TokenAuthentication, )
-    permission_classes = (permissions.IsAuthenticated, )
-
-    serializer_class = SerializerOntBaixa
-    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
-
-
-@api_view(['POST'])
+@api_view(['POST', ])
 def view_ont_baixa(request):
     """
     List all code snippets, or create a new snippet.
@@ -43,5 +35,16 @@ def view_ont_baixa(request):
         serializer = SerializerOntBaixa(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+            serial = serializer.validated_data.get('serial').upper()
+            saida_ont = OntSaida.objects.filter(ont__codigo=serial).latest('data')
+
+            response = {
+                'user_to': saida_ont.user_to.username,
+                'user_to_first_name': saida_ont.user_to.first_name,
+                'user_to_last_name': saida_ont.user_to.last_name,
+            }
+
+            return Response(response, status=status.HTTP_201_CREATED)
+
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
