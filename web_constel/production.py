c734909b8fe5f1f30dcc51dc7812495134d79a61
Gabriel Sa <gabrieljensensa@gmail.com>
2020-09-10 11:10:10 -0300

    Arrumando variável de produção

--- web_constel/production.py
+++ web_constel/production.py
@@ -7,9 +7,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 SECURE_SSL_REDIRECT = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-DEBUG = os.environ.get('DEBUG_VALUE', False)
+DEBUG = bool(os.environ.get('DEBUG_VALUE', False))
 
-ALLOWED_HOSTS = ['constel.herokuapp.com',]
+ALLOWED_HOSTS = ['constel.herokuapp.com']
 
 CONTWE2_TOKEN = os.environ.get('CONTWE2_TOKEN')
 CONTWE2_URL = os.environ.get('CONTWE2_URL')
