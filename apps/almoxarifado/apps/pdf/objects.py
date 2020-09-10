697a3c4f659f16cb809b3ad791fece78d3e7d400
Gabriel Sa <gabrieljensensa@gmail.com>
2020-09-10 12:16:16 -0300

    Arrumando varstatic files do pdf

--- apps/almoxarifado/apps/pdf/objects.py
+++ apps/almoxarifado/apps/pdf/objects.py
@@ -356,9 +356,9 @@ class FichaOntsManutencao(FPDF):
 
         self.set_font_size(10)
         # self.cell(self.epw, self.th * 2, cabecalho_texto, align='C', border=1)
-        self.image('apps\\almoxarifado\\apps\\pdf\\assets\\copel_telecom.jpg', h=4*self.th, w=self.epw*.2)
+        self.image('apps/almoxarifado/apps/pdf/assets/copel_telecom.jpg', h=4*self.th, w=self.epw*.2)
         self.set_xy(self.epw*.2 + self.l_margin, self.t_margin)
-        self.image('apps\\almoxarifado\\apps\\pdf\\assets\\titulo.jpg', h=4*self.th, w=self.epw*.5)
+        self.image('apps/almoxarifado/apps/pdf/assets/titulo.jpg', h=4*self.th, w=self.epw*.5)
         self.set_xy(self.l_margin, self.t_margin)
         self.cell(h=4 * self.th, w=self.epw * .2, border=1)
         self.cell(h=4 * self.th, w=self.epw * .5, border=1)
